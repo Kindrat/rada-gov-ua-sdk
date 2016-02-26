4bde61ce57c05bb2a8b160e91211386f71e4ea73
Kindrat <legioner@pp-ua.org>
2016-02-26 02:54:46 +0200

    compilation fix

--- shared/src/main/java/com/github/kindrat/radagovuasdk/client/dto/ListItem.java
+++ shared/src/main/java/com/github/kindrat/radagovuasdk/client/dto/ListItem.java
@@ -1,8 +1,14 @@
 package com.github.kindrat.radagovuasdk.client.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
 public class ListItem {
     private String title;
     private String uri;
